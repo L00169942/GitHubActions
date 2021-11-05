b0c3030dfd9169370476098f27d323aac0c6f967
Anup Jacob <L00163455@student.lyit.ie>
2021-11-05 10:54:14 +0000

    Second commit

---
+++ src/main/java/sample/GitActionsDemo.java
@@ -0,0 +1,11 @@
+package sample;
+
+public class GitActionsDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Welcome to LYIT");
+	}
+
+}
